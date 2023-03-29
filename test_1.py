211e74e18845e7264010879b38df44afc092faaa
sycat <sycat.sou@gmail.com>
2023-03-29 18:09:11 +0800

    Test headed mode browser.

--- test_1.py
+++ test_1.py
@@ -1,3 +1,8 @@
+from playwright.sync_api import Page
+import time
 
-def test_1_1(page):
-    page.goto('https://www.google.com')
+
+def test_1_1(page: Page):
+    page.goto('https://www.facebook.com/')
+    time.sleep(5)
+    page.screenshot(path='outputs/screenshot.png')
