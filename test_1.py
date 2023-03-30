35f389f82f4a34cc3147fc0cb9d6cc61e6e89e7b
sycat <sycat.sou@gmail.com>
2023-03-30 11:08:19 +0800

    Test screenshot.

--- test_1.py
+++ test_1.py
@@ -1,8 +1,26 @@
-from playwright.sync_api import Page
 import time
+from playwright.sync_api import Page, expect
 
 
 def test_1_1(page: Page):
-    page.goto('https://www.facebook.com/')
+    page.goto('https://www.facebook.com')
+    expect(page.get_by_test_id("royal_email")).to_be_visible()
+
+def test_1_2(page: Page):
+    page.goto('https://www.facebook.com', wait_until='load')
+    expect(page.get_by_test_id("royal_email")).to_be_visible()
+
+def test_1_3(page: Page):
+    page.goto('https://www.facebook.com', wait_until='domcontentloaded')
+    expect(page.get_by_test_id("royal_email")).to_be_visible()
+
+def test_1_4(page: Page):
+    page.goto('https://www.facebook.com', wait_until='networkidle')
+    expect(page.get_by_test_id("royal_email")).to_be_visible()
+
+def test_1_5(page: Page):
+    page.goto('https://www.facebook.com', wait_until='commit')
+    page.screenshot(path='outputs/test_1_5_1.png')
     time.sleep(5)
-    page.screenshot(path='outputs/screenshot.png')
+    page.screenshot(path='outputs/test_1_5_2.png')
+    expect(page.get_by_test_id("royal_email")).to_be_visible()
