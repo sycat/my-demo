baebc0e4c9921b7f95dcb5775bff411309394fc5
sycat <sycat.sou@gmail.com>
2023-04-14 16:46:47 +0800

    Test.

--- run_e2e_testing.sh
+++ run_e2e_testing.sh
@@ -1,2 +1,2 @@
-pip3 install --no-cache-dir -q -r requirements.txt --use-pep517
+pip install --no-cache-dir -q -r requirements.txt --use-pep517
 pytest --html=outputs/test_report.html --self-contained-html --junitxml=outputs/test_report.xml test_1.py
