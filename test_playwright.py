c4d551c057054908f735e7df653a1702997fe6fe
sycat <sycat.sou@gmail.com>
2023-07-12 15:20:20 +0800

    test action.

--- test_playwright.py
+++ test_playwright.py
@@ -4,7 +4,13 @@ from playwright.sync_api import Page, expect
 
 def test_playwright_1(page: Page):
     page.goto('https://www.google.com/')
+    page.get_by_text('Gmail').click()
+    time.sleep(3)
+    page.screenshot(path='outputs/screenshot_1.png')
+    assert True
 
 def test_playwright_2(page: Page):
-    page.goto('https://www.github.com/')
+    page.goto('https://www.instagram.com/')
+    time.sleep(3)
+    page.screenshot(path='outputs/screenshot_2.png')
     assert False
