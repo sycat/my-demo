0c1b4a242235da7c754aebb75263b3aea120de16
sycat <sycat.sou@gmail.com>
2023-09-05 16:18:37 +0800

    Report.

--- test_playwright.py
+++ test_playwright.py
@@ -6,11 +6,9 @@ def test_playwright_1(page: Page):
     page.goto('https://www.google.com/')
     page.get_by_text('Gmail').click()
     time.sleep(3)
-    page.screenshot(path='outputs/screenshot_1.png')
     assert True
 
 def test_playwright_2(page: Page):
     page.goto('https://www.instagram.com/')
     time.sleep(3)
-    page.screenshot(path='outputs/screenshot_2.png')
-    assert True
+    assert False
