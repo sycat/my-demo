91aa308c0d889e28d21be95177c5c732a9042c6f
sycat <sycat.sou@gmail.com>
2023-10-04 16:20:16 +0800

    Update CMD.

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,9 +1,11 @@
 import pytest
 
 
-def test_1_1():
+def test_1_1(page):
+    page.goto("https://www.google.com")
     assert True
 
 @pytest.mark.facebook
-def test_1_2():
+def test_1_2(page):
+    page.goto("https://www.facebook.com")
     assert True
