753407d2cc94f94bbcde2a90e12023cded9a4672
sycat <sycat.sou@gmail.com>
2023-11-08 13:51:53 +0800

    Add failed case

--- tests/test_1.py
+++ tests/test_1.py
@@ -2,12 +2,4 @@
 
 def test_1_1(page):
     page.goto("https://www.google.com")
-    assert True
-
-def test_1_2(page):
-    page.goto("https://www.google.com")
-    assert True
-
-def test_1_3(page):
-    page.goto("https://www.google.com")
-    assert True
+    assert False
