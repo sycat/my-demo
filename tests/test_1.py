4349bf1e6736b8e9e154b88c62bb8d1d78bae4df
sycat <sycat.sou@gmail.com>
2023-11-08 11:47:37 +0800

    Fix test case and update dependencies

--- tests/test_1.py
+++ tests/test_1.py
@@ -3,9 +3,4 @@ import pytest
 
 def test_1_1(page):
     page.goto("https://www.google.com")
-    assert True
-
-@pytest.mark.facebook
-def test_1_2(page):
-    page.goto("https://www.facebook.com")
-    assert True
+    assert False
