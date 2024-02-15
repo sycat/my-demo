6b6b3cdef4947d07859f174653ea07c4a0ae88da
sycat <sycat.sou@gmail.com>
2024-02-15 17:30:54 +0800

    Test redirect url

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,7 +1,11 @@
+import re
 import pytest
+from playwright.sync_api import Page, expect
 
 
-@pytest.mark.parametrize("i", range(50))
-def test_num(i):
-    if i in (17, 25):
-        pytest.fail("bad luck")
+def test_home(page):
+    page.goto('https://stg.idprotect.trendmicro.com/ja')
+    with page.context.expect_page() as new_page_info:
+        page.locator("div").filter(has_text=re.compile(r"^個人情報盗難のリスクを評価$")).first.click(timeout=15000)
+    new_page = new_page_info.value
+    expect(new_page).to_have_url(re.compile(r'https://helpcenter\.trendmicro\.com/ja-jp/dwa/idp/assessment/.*'), timeout=20000)
